84944f338f211920d58cd0f81149e4b91e0f3eb0
v4z <luizsamuelmaciel@gmail.com>
2021-12-07 21:08:04 -0300

    first commit

---
+++ src/main/java/br/com/tech4me/tech4cars/model/Carro.java
@@ -0,0 +1,53 @@
+package br.com.tech4me.tech4cars.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("carros")
+public class Carro {
+    @Id
+    private String id;
+    private String modelo;
+    private String marca;
+    private String placa;
+    private Double valor;
+   
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public String getPlaca() {
+        return placa;
+    }
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+    public Double getValor() {
+        return valor;
+    }
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+   
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public boolean isPresent() {
+        return false;
+    }
+   
+
+    
+    
+}
